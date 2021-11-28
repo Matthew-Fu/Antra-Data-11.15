d67080eef50ee69f1883f13369fc201beedf8f1b
Zhe Fu <72235751+Matthew-Fu@users.noreply.github.com>
2021-11-28 13:43:24 -0500

    Add files via upload

---
+++ 13.sql
@@ -0,0 +1,16 @@
+/*List of stock item groups and total quantity purchased, total quantity sold, and 
+the remaining stock quantity (quantity purchased – quantity sold) */
+
+use WideWorldImporters
+go
+select ws.stockitemid, a.quantity_order, a.quantity_sale, a.quantity_order-a.quantity_sale as remaining_stock_quantity 
+from Warehouse.StockItems ws
+join
+(select ws.StockItemID,sum(ppl.OrderedOuters) as quantity_order,sum(sol.Quantity) as quantity_sale
+from Warehouse.StockItems ws
+join Purchasing.PurchaseOrderLines ppl
+on ws.StockItemID =ppl.StockItemID
+join Sales.OrderLines sol
+on ppl.StockItemID=sol.StockItemID
+group by ws.StockItemID) a 
+on ws.stockitemid = a.StockItemID
---
+++ 14.sql
@@ -0,0 +1,20 @@
+/* List of Cities in the US and the stock item that the city got the most deliveries in 2016. 
+If the city did not purchase any stock items in 2016, print “No Sales”. */
+
+use WideWorldImporters
+go
+select us.CityName, us.StateProvinceName, 
+ISNULL(cast(StockItemID as varchar), 'No Sales') most_delivered_stockitem
+from
+(select CityID, cityname, StateProvinceName, StockItemID, count(InvoiceID) deliveries,
+rank() over (partition by cityname, StateProvinceName order by count(InvoiceID) desc) rank_d
+from Sales.Invoices i 
+join Sales.OrderLines sol on i.OrderID = sol.OrderID
+join Sales.Orders so on so.OrderID = sol.OrderID
+join Sales.Customers sc on sc.CustomerID = so.CustomerID 
+right join Application.Cities ac  on DeliveryCityID = ac.cityid
+join Application.StateProvinces sp on sp.StateProvinceID = ac.StateProvinceID
+join Application.Countries co on co.CountryID = sp.CountryID 
+where year(OrderDate) = 2016
+group by CityID, cityname, StateProvinceName, StockItemID ) us
+order by StateProvinceName, CityName
