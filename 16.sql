056c66bcc898723b44209583a70f162c5a42b70a
Zhe Fu <72235751+Matthew-Fu@users.noreply.github.com>
2021-11-28 13:46:43 -0500

    Add files via upload

---
+++ 16.sql
@@ -0,0 +1,7 @@
+/*List all stock items that are manufactured in China. 
+(Country of Manufacture)*/
+use WideWorldImporters
+go
+select StockItemID
+from Warehouse.StockItems
+where json_value(CustomFields,'$.CountryOfManufacture') = 'China'
